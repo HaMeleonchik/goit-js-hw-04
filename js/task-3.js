1ee2308960210812336368fdc825bfa195c0049d
HaMeleonchik <tntshow.tiktok@gmail.com>
2025-02-11 18:55:05 +0200

    hw04js

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,23 @@
-function filterArray(numbers, value) {
-    let successfulvalue = [];
-    for(let i = 0; i < numbers.length; i++ ){
-        if (numbers[i] > value) {
-            successfulvalue.push(numbers[i]);
-        }
-    }
-return successfulvalue;
-};
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    getInfo: function(){
+return `${this.username} has ${this.playTime} active hours!`;
+    },
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+    changeUsername: function (newUsername) {
+       this.username = newUsername;
+    },
+    updatePlayTime: function (superTime) {
+      this.playTime += superTime;
+    },
+    
+    };
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
